7b39bf47fe6961b2146059d981147e8c6d5d0367
KevsCha <k-e-v-i-n_13_@hotmail.com>
2026-02-25 10:45:47 +0100

    primer commit en rama Kevs

--- src/calculadora/CalculadoraMain.java
+++ src/calculadora/CalculadoraMain.java
@@ -37,7 +37,8 @@ public class CalculadoraMain {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	
 	}
 
 }
