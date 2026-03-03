73ff232638dc7327ee996f9c581a087f6b9675af
madrid <madrid@192.168.56.1>
2026-03-03 20:07:13 +0100

    commit Rebe

--- src/calculadora/CalculadoraMain.java
+++ src/calculadora/CalculadoraMain.java
@@ -1,12 +1,17 @@
 package calculadora;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-public class CalculadoraMain {
+public class CalculadoraMain implements ActionListener{
 
 	private JFrame frame;
+	private JTextField campoTexto;
 
 	/**
 	 * Launch the application.
@@ -39,6 +44,13 @@ public class CalculadoraMain {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton boton=(JButton)e.getSource();
+		String numero = campoTexto.getText()+boton.getText();
+		campoTexto.setText(numero);
 	}
 
 }
